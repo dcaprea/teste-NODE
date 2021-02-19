2e7bb2c9e39ec30a3abdd11b9ecb69d25175b3fa
Daniel Aprea <daniel.aprea@opah.com.br>
2021-02-19 05:12:39 -0300

    Mudando a view para EJS e usado NODEMON

--- app.js
+++ app.js
@@ -4,13 +4,22 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
+
 app.get('/', function(req, res){
-    res.send("<html><body>Portal de Notícias</body></html>");
+    res.render("home/index");
 });
 
-app.get('/tecnologia', function(req, res){
-    res.send("<html><body>Portal de Notícias - Tecnologia</body></html>");
+app.get('/add', function(req, res){
+    res.render("admin/form_add_noticia");
 });
+
+app.get('/noticias', function(req, res){
+    res.render('noticias/noticias');
+});
+
+
 
 app.listen(3000, function(){
     console.log("Servidor Rodando com Express");
